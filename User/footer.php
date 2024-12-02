8c2f388b596e9fe17dbd8f12440d1e536051715e
Wei Quan <67322838+The-truth-seeker69@users.noreply.github.com>
2024-12-02 21:22:55 +0800

    changed the homepage to bookstore

---
+++ User/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+    <div class="footer-container">
+
+        <div class="footer-section">
+            <h4>About Us</h4>
+            <p>Welcome to Unpopular, where stories come to life. Explore our diverse collection of books for every reader, from timeless classics to modern favorites. Your next great adventure starts here!</p>
+        </div>
+
+        <div class="footer-section">
+            <h4>Contact Us</h4>
+            <p>Email: support@popular.com</p>
+            <p>Phone: +123-456-7890</p>
+            <p>Address: 123 Fashion Street, Trendy City</p>
+        </div>
+
+        <div class="footer-section">
+            <h4>Follow Us</h4>
+            <div class="social-links">
+                <a href="#" target="_blank">Facebook</a>
+                <a href="#" target="_blank">Instagram</a>
+                <a href="#" target="_blank">Twitter</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Badini. All rights reserved.</p>
+    </div>
+</footer>
+
+</html>
---
+++ User/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page </title>
+    <link rel="stylesheet" href="../Style/home.css">
+    <link rel="stylesheet" href="../Style/header.css">
+    <link rel="stylesheet" href="../Style/footer.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+
+
+        <div id="header-brand">
+            <h1><a href="/">Unpopular </a></h1>
+        </div>
+
+        <ul class="nav-links">
+            <li><a href="#">Fiction</a></li>
+            <li><a href="#">Non-fiction</a></li>
+            <li><a href="#">Kids's book</a></li>
+            <li><a href="#">放爽</a></li>
+        </ul>
+        <div id="header-utility">
+            <a href="#login" class="nav-item">
+                <i class="fas fa-user"></i> Login / Register
+            </a>
+
+            <a href="#cart" class="nav-item">
+                <i class="fas fa-shopping-bag"></i>
+            </a>
+        </div>
+
+    </header>
+
+    </nav>
