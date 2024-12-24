ab33b9958148cb022eb133b645a2087afaf616c9
jinghui8866 <jinghuiwong0815@gmail.com>
2024-12-24 09:52:49 +0800

    p

--- User/page/footer.php
+++ User/page/footer.php
@@ -26,6 +26,10 @@
     <div class="footer-bottom">
         <p>&copy; 2024 Badini. All rights reserved.</p>
     </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.5428949349675!2d101.72400367447047!3d3.213953552751904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc386bb5535b83%3A0x2d08b6c5b6fd0155!2sTAR%20UMT%20Cyber%20Centre%20(CITC)!5e0!3m2!1sen!2smy!4v1735001733119!5m2!1sen!2smy" 
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" 
+    referrerpolicy="no-referrer-when-downgrade">
+</iframe>
 </footer>
 
 </html>
